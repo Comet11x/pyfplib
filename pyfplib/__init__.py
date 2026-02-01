22425e0e2f0321e599506bd88e8ac8b7735b3cef
comet11x <comet11x@protonmail.com>
2026-02-02 02:39:27 +0300

    feat!: update __init__ file

--- pyfplib/__init__.py
+++ pyfplib/__init__.py
@@ -1,85 +1,29 @@
 # SPDX-FileCopyrightText: 2026-present Comet11x
 # SPDX-License-Identifier: MIT
 
-from collections.abc import Callable, Iterable, Sequence
-from typing import TypeVar
-
 from pyfplib.either import Either, Left, Right
+from pyfplib.functions import all_of, any_of, fold, for_each, head, is_empty, is_not_empty, last, none_of, tail
 from pyfplib.option import Nothing, Option, Some
 from pyfplib.result import Err, Ok, Result
 
-T = TypeVar("T")
-
-
-def one_of(iterable: Iterable[T], condition: Callable[[T], bool]) -> bool:
-    """\
-    Returns True if the callback function returns True
-    for one of elements of an iterable.
-    """
-    cond = False
-    for elem in iterable:
-        if condition(elem):
-            cond = True
-            break
-    return cond
-
-
-def all_of(iterable: Iterable[T], callback: Callable[[T], bool]) -> bool:
-    """\
-    Returns True if the callback function returns True
-    for all of elements of an iterable.
-    """
-    cond = False
-    for elem in iterable:
-        cond = callback(elem)
-        if not cond:
-            break
-    return cond
-
-
-def first(sequence: Sequence[T]) -> Option[T]:
-    """\
-    Returns Some[T] with 1st element of the sequence
-    if the length of the sequence is not equal 0.
-    If the length of the sequence is equal 0 this function returns Nothing[T].
-    """
-    o: Option
-    if len(sequence) == 0:
-        o = Nothing[T]()
-    else:
-        o = Some[T](sequence[0])
-    return o
-
-
-def last(sequence: Sequence[T]) -> Option[T]:
-    """\
-    Returns Some[T] with last element of the sequence
-    if the length of the sequence is not equal 0.
-    If the length of the sequence is equal 0 this function returns Nothing[T].
-    """
-    o: Option
-    if len(sequence) == 0:
-        o = Nothing[T]()
-    else:
-        o = Some[T](sequence[-1])
-    return o
-
-
 __all__ = (
-    "Either",
-    "Err",
-    "Left",
-    "Nothing",
-    "Ok",
-    "Option",
-    "Result",
-    "Right",
-    "Some",
-    "all_of",
-    "first",
-    "from_optional",
-    "from_result",
-    "last",
-    "one_of",
-    "try_call",
+    Either.__name__,
+    Err.__name__,
+    Left.__name__,
+    Nothing.__name__,
+    Ok.__name__,
+    Option.__name__,
+    Result.__name__,
+    Right.__name__,
+    Some.__name__,
+    all_of.__name__,
+    any_of.__name__,
+    none_of.__name__,
+    fold.__name__,
+    for_each.__name__,
+    head.__name__,
+    tail.__name__,
+    last.__name__,
+    is_empty.__name__,
+    is_not_empty.__name__,
 )
