53510f08a92d449e748ca0d6ba2af12014193dc5
comet11x <comet11x@protonmail.com>
2026-02-02 02:16:55 +0300

    feat: update result

--- pyfplib/result.py
+++ pyfplib/result.py
@@ -1,28 +1,35 @@
-"""This module provides Result[T] type
-and two constructors of Option: Ok[T] and Err[T].
 """
+"""
+__author__ = "Comet11x <>"
+__copyright__ = "Copyright 2026, Comet11x"
+__license__ = "MIT"
+__version__ = "0.1.0"
 
-from typing import Any, Callable, Generic, TypeVar, cast
+from collections.abc import Callable
+from typing import Generic, Optional, TypeVar, Union, cast
 
-from mypy_extensions import KwArg, VarArg
-
+from pyfplib.errors import ExpectedError, UnwrapError
 from pyfplib.option import Nothing, Option, Some
 
 T = TypeVar("T")
+E = TypeVar("E")
 R = TypeVar("R")
 U = TypeVar("U")
 
 
-class _Result(Generic[T]):
-
+class Result(Generic[T, E]):
     __match_args__ = ("value",)
 
-    def __init__(self, value: T | Exception):
-        self.__value = value
-        self.__is_ok = not isinstance(value, Exception)
+    def __init__(self, *, value: Optional[T] = None, error: Optional[E] = None):
+        if error is None:
+            self.__value = value
+            self.__is_ok = True
+        else:
+            self.__value = error
+            self.__is_ok = False
 
     @property
-    def value(self) -> T | Exception:
+    def value(self) -> Union[T, E]:
         """Returns a raw value."""
         return self.__value
 
@@ -34,22 +41,22 @@ class _Result(Generic[T]):
         """Returns True if the option is Err[T]."""
         return not self.__is_ok
 
-    def if_ok(self, fn: Callable[[T], None]) -> "Result[T]":
+    def if_ok(self, fn: Callable[[T], None]) -> "Result[T, E]":
         """\
         Calls fn function and returns itself.
         The fn function will be called if this option is Ok[T].
         """
         if self.is_ok():
-            fn(cast(T, self.__value))
+            fn(self.__value)
         return self
 
-    def if_err(self, fn: Callable[[Exception], None]) -> "Result[T]":
+    def if_err(self, fn: Callable[[E], None]) -> "Result[T, E]":
         """\
         Calls fn function and returns itself.
-        The fn function will be called if this option is Err[T].
+        The fn function will be called if this option is Err[E].
         """
         if self.is_err():
-            fn(cast(Exception, self.__value))
+            fn(self.__value)
         return self
 
     def ok(self) -> Option[T]:
@@ -57,11 +64,11 @@ class _Result(Generic[T]):
         Returns Some[T] if this option is Ok[T],
         otherwise it returns Nothing[T].
         """
-        return Some[T](cast(T, self.__value)) if self.is_ok() else Nothing[T]()
+        return Some(self.__value) if self.is_ok() else Nothing()
 
-    def err(self) -> Option[Exception]:
+    def err(self) -> Option[E]:
         """\
-        Returns Some[Exception] if this option is Err[T],
+        Returns Some[Exception] if this option is Err[E],
         otherwise it returns Nothing[Exception].
         """
         option: Option[Exception]
@@ -71,48 +78,86 @@ class _Result(Generic[T]):
             option = Nothing[Exception]()
         return option
 
-    def map(self, fn: Callable[[T], U]) -> "Result[U]":
+    def unwrap(self) -> T:
+        """
+        Extracts the contained value, raising UnwrapError if Err.
+
+        Raises:
+            UnwrapError: When attempting to unwrap Err
+        """
+        if self.is_ok():
+            msg = "called `Result.unwrap()` on a `Err` value"
+            raise UnwrapError(msg)
+        return self.__value
+
+    def unwrap_err(self) -> E:
+        """
+        Extracts the contained error, raising UnwrapError if Ok.
+
+        Raises:
+            UnwrapError: When attempting to unwrap Ok
+        """
+        if self.is_err():
+            msg = "called `Result.unwrap_err()` on a `Ok` value"
+            raise UnwrapError(msg)
+        return self.__value
+
+    def expect(self, message: str) -> T:
+        """Returns contained value or raises ExpectedError"""
+        if self.is_err():
+            raise ExpectedError(message)
+        return self.__value
+
+    def expect_err(self, message: str) -> E:
+        """Returns contained value or raises ExpectedError"""
+        if self.is_ok():
+            raise ExpectedError(message)
+        return self.__value
+
+    def unwrap_or(self, value: T) -> T:
+        """Returns contained value or provided default if Err."""
+        return self.__value if self.is_ok() is None else value
+
+    def unwrap_err_or(self, value: E) -> E:
+        """Returns contained value or provided default if Ok."""
+        return self.__value if self.is_err() is None else value
+
+    def map(self, fn: Callable[[T], U]) -> "Result[U, E]":
+        """Maps an Result[T, E] to Result[U, E]"""
+        result: Result[U, E]
+        if self.is_ok():
+            result = Ok[U, E](fn(cast(T, self.__value)))
+        else:
+            result = Err[U, E](cast(E, self.__value))
+        return result
+
+    def map_or(self, default: T, fn: Callable[[T], U]) -> "Result[U]":
         """Maps an Result[T] to Result[U]"""
         result: Result[U]
         if self.is_ok():
-            result = Ok[U](fn(cast(T, self.__value)))
+            result = fn(self.__value)
         else:
-            result = Err[U](cast(Exception, self.__value))
+            result = fn(default)
         return result
 
-    def map_from(self, fn: Callable[[T], "Result[U]"]) -> "Result[U]":
-        """Maps an Result[T] to Result[U]"""
-        result: Result[U]
-        if self.is_ok():
-            result = fn(cast(T, self.__value))
-        else:
-            result = Err[U](cast(Exception, self.__value))
-        return result
+    @staticmethod
+    def try_call(fn: Callable[..., R], *args, **kwargs) -> "Result[R, E]":
+        """Tries to call fn function and returns a result of the execution."""
+        try:
+            ret: R = fn(*args, **kwargs)
+            return Ok[R, E](ret)
+        except Exception as err:
+            return Err[R, E](err)
 
-
-class Ok(_Result[T]):
+class Ok(Result[T, E]):
     """Result constructor."""
 
     def __init__(self, value: T):
-        super().__init__(value)
+        super().__init__(value=value)
 
 
-class Err(_Result[T]):
+class Err(Result[T, E]):
     """Result constructor."""
 
-    def __init__(self, error: Exception):
-        super().__init__(error)
-
-
-def try_call(fn: Callable[[VarArg(Any), KwArg(Any)], R], *args, **kwargs) -> "Result[R]":
-    """Tries to call fn function and returns a result of the execution."""
-    try:
-        ret: R = fn(*args, **kwargs)
-        return Ok[R](ret)
-    except Exception as err:
-        return Err[R](err)
-
-
-Result = Ok[T] | Err[T]
-
-__all__ = ("Ok", "Err")
+    def __init__(self, error: E):
+        super().__init__(error=error)
