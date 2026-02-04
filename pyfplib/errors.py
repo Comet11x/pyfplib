5fe460de3da6f5c443a312e03073fa534e1b784a
comet11x <comet11x@protonmail.com>
2026-02-04 22:52:24 +0300

    feat!: move files

--- pyfplib/errors.py
+++
@@ -1,9 +0,0 @@
-"""This module provides errors of pyfplib."""
-
-
-class ExpectedError(Exception):
-    """ExpectedError is used in `expect` methods as Exception"""
-
-
-class UnwrapError(Exception):
-    """UnwrapError extends Exception."""
