93e1991a5caf2897d9b04384f0651439250172ce
comet11x <comet11x@protonmail.com>
2026-02-04 23:03:23 +0300

    feat(test): update tests

--- tests/result/test_result.py
+++ tests/result/test_result.py
@@ -1,16 +1,18 @@
 import pytest
 
-from pyfplib import Err, Ok, Result, first
+from pyfplib import Ok, Result
 
 
 def create_result() -> Result:
     return Ok("TEST")
 
+
 def test_result():
     result = create_result()
-    value = "TEST"
-    match result:
-        case Ok(value):
-            assert result.unwrap() == value
-        case Err(Exception("TEST")):
-            pytest.fail()
+    assert result.is_ok()
+    # value = "TEST"
+    # match result:
+    #    case Ok(value):
+    #       assert result.unwrap() == value
+    #    case Err(Exception("TEST")):
+    #        pytest.fail()
